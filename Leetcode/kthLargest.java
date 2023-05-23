aff1617f8d3e7208af5bd5d7c7a0117b49875277
Devesh Parwani <devesh096@gmail.com>
2023-05-23 15:16:31 +0530

    leetcode lcdp added

---
+++ Leetcode/kthLargest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class kthLargest {
+    class KthLargest {
+        private PriorityQueue<Integer> heap = new PriorityQueue<>();
+        private int k;
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            for (var n : nums)
+                add(n);
+        }
+
+        public int add(int val) {
+            heap.offer(val);
+            if (heap.size() > k)
+                heap.poll();
+            return heap.peek();
+        }
+    }
+
+}
