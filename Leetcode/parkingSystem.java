59661f7569d6c7c54896c559256094cdccae9439
Devesh Parwani <devesh096@gmail.com>
2023-05-29 07:06:43 +0530

    leetcode lcdp added

---
+++ Leetcode/parkingSystem.java
@@ -0,0 +1,23 @@
+public class parkingSystem {
+    class ParkingSystem {
+        int big, medium, small;
+
+        public ParkingSystem(int big, int medium, int small) {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+
+        public boolean addCar(int carType) {
+            switch (carType) {
+                case 1:
+                    return big-- > 0;
+                case 2:
+                    return medium-- > 0;
+                case 3:
+                    return small-- > 0;
+            }
+            return false;
+        }
+    }
+}
