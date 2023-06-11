f6e44bf222554455de709462b33fae195556df62
Devesh Parwani <devesh096@gmail.com>
2023-06-11 14:50:23 +0530

    leetcode lcdp added

---
+++ GFG/addingOnes.java
@@ -0,0 +1,15 @@
+package GFG;
+
+public class addingOnes {
+    class Solution {
+
+        public static void update(int a[], int n, int updates[], int k) {
+            for (int i = 0; i < k; i++)
+                a[updates[i] - 1]++;
+
+            for (int i = 1; i < n; i++)
+                a[i] += a[i - 1];
+        }
+
+    }
+}
