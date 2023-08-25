a54b146dd1c078bd6d011d10a513a23b33409041
Devesh Parwani <devesh096@gmail.com>
2023-08-25 23:07:17 +0530

    git upload

---
+++ GFG/palindromeString.java
@@ -0,0 +1,14 @@
+package GFG;
+
+public class palindromeString {
+    class Solution {
+        int isPalindrome(String S) {
+            // code here
+            for (int i = 0; i < S.length() / 2; i++)
+                if (S.charAt(i) != S.charAt(S.length() - i - 1))
+                    return 0;
+
+            return 1;
+        }
+    };
+}
